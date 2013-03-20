541c7a14c55afde87c61f813a307000cde22f2d7
Tim Santeford <tsantef@gmail.com>
2013-03-19 20:01:18 -0700

    Fixes login problems

--- appfog/lib/appfog-vmc-plugin/non_uaa.rb
+++ appfog/lib/appfog-vmc-plugin/non_uaa.rb
@@ -23,23 +23,15 @@ module CFoundry
 
   module LoginHelpers
     def login_prompts
-      if @base.uaa
-        @base.uaa.prompts
-      else
-        {
-          :username => %w[text Email],
-          :password => %w[password Password]
-        }
-      end
+      {
+        :username => %w[text Email],
+        :password => %w[password Password]
+      }
     end
 
     def login(username, password)
       token =
-        if @base.uaa
-          AuthToken.from_uaa_token_info(@base.uaa.authorize(username, password))
-        else
-          AuthToken.from_cc_token(@base.create_token({:password => password}, username)[:token])
-        end
+        AuthToken.from_cc_token(@base.create_token({:password => password}, username)[:token])
 
       @base.token = token
     end
